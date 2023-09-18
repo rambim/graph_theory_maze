1e3c02e10a3496173053c3d59a3357c075eacb88
RaulDelary <rauldelary@gmail.com>
2023-09-18 17:20:20 -0300

    feat: add Dockerfile and docker-compose

--- api/config/config.py
+++ api/config/config.py
@@ -1,8 +1,8 @@
-# from pydantic_settings import BaseSettings
+import os
 
 TIME_IN_SEC = 60 * 5
 
 class Config:
-  GTM_REDIS_HOST: str = 'localhost'
-  GTM_REDIS_PORT: int = 6379
-  GTM_SESSION_TTL: int = TIME_IN_SEC
+  GTM_REDIS_HOST: str = os.getenv ('GTM_REDIS_HOST', 'localhost')
+  GTM_REDIS_PORT: int = int (os.getenv ('GTM_REDIS_PORT', 6379))
+  GTM_SESSION_TTL: int = int (os.getenv ('GTM_SESSION_TTL', TIME_IN_SEC))
